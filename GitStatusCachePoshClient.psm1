cb0b3bfbbf941dcac09b0d5189ce11e1797ffc3c
Marcus Reid <marcus.reid@live.com>
2015-09-03 13:59:43 -0700

    Import Microsoft.PowerShell.Utility if it isn't loaded.

--- GitStatusCachePoshClient.psm1
+++ GitStatusCachePoshClient.psm1
@@ -1,5 +1,10 @@
 if (Get-Module GitStatusCachePoshClient) { return }
 
+if (-not (Get-Module Microsoft.Powershell.Utility))
+{
+	Import-Module Microsoft.Powershell.Utility
+}
+
 Push-Location $psScriptRoot
 . .\GitStatusCachePoshClient.ps1
 Pop-Location
