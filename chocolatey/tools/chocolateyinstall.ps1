6252fcb75ca13a927c912dde47766659fd9b30ae
Marcus Reid <marcus.reid@live.com>
2015-09-08 02:21:26 +0100

    Create basic chocolatey package.

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'git-status-cache-posh-client'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://github.com/cmarcusreid/git-status-cache-posh-client/archive/v0.1.3.zip'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+$subfolder = get-childitem $toolsDir -recurse -include 'git-status-cache-posh-client-0.1.3' | select -First 1
+$installer = Join-Path $subfolder 'install.ps1'
+& $installer
