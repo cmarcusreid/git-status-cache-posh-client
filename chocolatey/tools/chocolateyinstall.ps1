74ac20b6fd5cd14113c791842f56d85919e7ae18
Marcus Reid <marcus.reid@live.com>
2015-09-11 05:33:43 +0100

    Set version number to 1.0.0.

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageName = 'git-status-cache-posh-client'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/cmarcusreid/git-status-cache-posh-client/archive/v0.1.3.zip'
+$url = 'https://github.com/cmarcusreid/git-status-cache-posh-client/archive/v1.0.0.zip'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
 
-$subfolder = get-childitem $toolsDir -recurse -include 'git-status-cache-posh-client-0.1.3' | select -First 1
+$subfolder = get-childitem $toolsDir -recurse -include 'git-status-cache-posh-client-1.0.0' | select -First 1
 $installer = Join-Path $subfolder 'install.ps1'
 & $installer
--- chocolatey/tools/chocolateyuninstall.ps1
+++ chocolatey/tools/chocolateyuninstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'git-status-cache-posh-client'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$subfolder = get-childitem $toolsDir -recurse -include 'git-status-cache-posh-client-0.1.3' | select -First 1
+$subfolder = get-childitem $toolsDir -recurse -include 'git-status-cache-posh-client-1.0.0' | select -First 1
 $uninstaller = Join-Path $subfolder 'uninstall.ps1'
 & $uninstaller
 
